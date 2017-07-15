b06e8eb77923893164f6952aab378dea300e5a42
Choe <qjarbs111@gmail.com>
2017-07-15 19:05:43 +0900

    10815

--- src/main/java/week_7/NumberCard_10815.java
+++ src/main/java/week_7/NumberCard_10815.java
@@ -3,6 +3,7 @@ package week_7;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
@@ -12,22 +13,38 @@ public class NumberCard_10815 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] card1 = new int[N];
+        int[] card = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i=0; i<N; i++) {
-            card1[i] = Integer.parseInt(st.nextToken());
+            card[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(card);
         int M = Integer.parseInt(br.readLine());
-        int[] card2 = new int[M];
         st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for (int i=0; i<M; i++) {
-            card2[i] = Integer.parseInt(st.nextToken());
+            sb.append(binarySearch(card, Integer.parseInt(st.nextToken()))).append(" ");
         }
-        binarySearch(card1, card2);
+        System.out.println(sb.toString());
         br.close();
     }
 
-    private static void binarySearch(int[] card1, int[] card2) {
+    private static int binarySearch(int[] card, int num) {
+        int front = 0;
+        int rear = card.length - 1;
+        int mid;
 
+        while (front <= rear) {
+            mid = (front + rear) / 2;
+
+            if (card[mid] == num) {
+                return 1;
+            } else if (card[mid] < num) {
+                front = mid + 1;
+            } else if (card[mid] > num) {
+                rear = mid - 1;
+            }
+        }
+        return 0;
     }
 }
